31be32940a854a52a8cc7b9dfb015013631ecfcd
tatomyr <andrew.tatomyr@gmail.com>
2016-10-28 12:45:22 +0300

    Revert "let -> var test"
    This reverts commit e6d3d88a457af8f6e9e24354ada336fe3bc6b9be.

--- scrollover.js
+++ scrollover.js
@@ -20,7 +20,7 @@ const scrollover = (scrolloverTime) => {
         dY = Math.round(event.originalEvent.deltaY); // Vanilla.js: event.deltaY, jQuery: event.originalEvent.deltaY
       console.log(`t: ${t}, T: ${T} | h: ${h}, H: ${H} | dY: ${dY}`);
 
-      var scrollTop = undefined;
+      let scrollTop = undefined;
       if (dY > 0) {
         if (t > T) {
           scrollTop = t;
